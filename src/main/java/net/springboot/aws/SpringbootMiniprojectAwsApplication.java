119ebd25b77472c2262972426f58e3d25f00bf41
Manikantabellamkonda <manikantabellamkonda09@gmail.com>
2022-03-30 15:51:45 +0530

    Mani Commit

---
+++ src/main/java/net/springboot/aws/SpringbootMiniprojectAwsApplication.java
@@ -0,0 +1,13 @@
+package net.springboot.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMiniprojectAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMiniprojectAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/springboot/aws/SpringbootMiniprojectAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springboot.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMiniprojectAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
